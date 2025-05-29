66a05d2c861d131f2f98e6c530d834fa86428684
HapiFive <1143910315@qq.com>
2025-05-29 19:50:31 +0800

    添加项目文件。

---
+++ QuickSend/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace QuickSend
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ QuickSend/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace QuickSend
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
