c0b1f957f024a229ce4a8d9047c29be0c258c62b
HapiFive <1143910315@qq.com>
2025-05-31 08:04:24 +0800

    udp服务器

---
+++ QuickSend/Network/UdpHighPerfServer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace QuickSend.Network {
+    internal class UdpHighPerfServer {
+        private readonly Socket _socket;
+        private readonly ArrayPool<byte> arrayPool = ArrayPool<byte>.Shared;
+        private bool _isRunning = false;
+        private readonly Channel<UdpInfo> channel = Channel.CreateUnbounded<UdpInfo>();
+        public event EventHandler<UdpInfo>? OnReceived;
+        public UdpHighPerfServer(int port) {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            _socket.Bind(new IPEndPoint(IPAddress.Any, port));
+            _socket.DontFragment = true;
+        }
+
+        public void Start() {
+            _isRunning = true;
+            Task.Run(ReceiveLoop);
+            Task.Run(ProcessLoop);
+        }
+
+        private async Task ReceiveLoop() {
+            while (_socket.Poll(-1, SelectMode.SelectRead) && _socket.Available > 0) {
+                // 从内存池租用缓冲区
+                byte[] buffer = arrayPool.Rent(_socket.Available);
+                // 零拷贝接收
+                var result = await _socket.ReceiveFromAsync(
+                    buffer,
+                    SocketFlags.None,
+                    new IPEndPoint(IPAddress.Any, 0)
+                ).ConfigureAwait(false);
+
+                // 直接处理数据（示例：原样回传）
+                await channel.Writer.WriteAsync(new UdpInfo(buffer, result.ReceivedBytes, result.RemoteEndPoint)).ConfigureAwait(false);
+            }
+            Debug.WriteLine("ReceiveLoop stopped");
+        }
+
+        // 处理数据包（零拷贝处理）
+        private async Task ProcessLoop() {
+            while (_isRunning) {
+                UdpInfo udpInfo = await channel.Reader.ReadAsync().ConfigureAwait(false);
+                OnReceived?.Invoke(this, udpInfo);
+                arrayPool.Return(udpInfo.Data);
+            }
+        }
+
+        // 异步发送（使用原始内存块）
+        public async ValueTask<int> SendAsync(Memory<byte> data, EndPoint remoteEP) {
+            return await _socket.SendToAsync(data, SocketFlags.None, remoteEP);
+        }
+
+        public void Dispose() {
+            _isRunning = false;
+            _socket?.Dispose();
+        }
+    }
+}
---
+++ QuickSend/Network/UdpInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSend.Network {
+    internal class UdpInfo(byte[] data, int dataLength, EndPoint remoteEndPoint) {
+        private readonly byte[] data = data;
+        private readonly int dataLength = dataLength;
+        private readonly EndPoint remoteEndPoint = remoteEndPoint;
+
+        public byte[] Data => data;
+
+        public int DataLength => dataLength;
+
+        public EndPoint RemoteEndPoint => remoteEndPoint;
+    }
+}
